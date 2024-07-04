400168032bbadb80d66f7b460f4a8e3806c561b7
Merve Ozturk <ozturkmerve745@gmail.com>
2024-07-04 16:02:07 +0300

    new algorithm written and commented

--- algorithmPractice/algorithmPractice.js
+++ algorithmPractice/algorithmPractice.js
@@ -1,4 +1,3 @@
-```js
 function findSameLetters(string) {
   // Bir fonksiyon oluşturuyoruz ve bu fonksiyona parametre olarak bir string veriyoruz.
 
@@ -27,7 +26,6 @@ function findSameLetters(string) {
 
 console.log(findSameLetters('abcbc')); // Fonksiyonu 'abcbc' stringi ile test ediyoruz ve ilk tekrar eden karakteri konsola yazdırıyoruz.
 
-```
 function findSmallestMissingPositiveInteger(array) {
   // Bir fonksiyon oluşturuyoruz ve bu fonksiyona parametre olarak bir dizi veriyoruz.
 
@@ -51,3 +49,23 @@ function findSmallestMissingPositiveInteger(array) {
 }
 
 console.log(findSmallestMissingPositiveInteger([-3, -2, -1, 0, 1])); // Fonksiyonu [-3, -2, -1, 0, 1] dizisi ile test ediyoruz ve sonucu konsola yazdırıyoruz.
+
+function firstUnlimitedCharacter(string) {
+  // fonksiyon oluşturuyoruz ve fonskiyona string değer atıyoruz
+
+  for (const char of string) {
+    // String içindeki her bir karakter için döngü oluşturuyoruz.
+    if (string.split('').filter(c => c === char).length === 1) {
+      // string.split('') ile stringi karakterlerine ayırıyoruz.
+      // c burada stringin herhangi bir karakteri temsil ediyor
+      // char burada ilk tekrar eden karakteri temsil ediyor
+      // her bir karakteri char ile karşılaştırarak filtreliyoruz ve char ile aynı olan karakteri seçiyoruz
+      // eğer seçilen karakter uzunluğu 1 e eşitse ilk benzersiz karakteri döndürüyoruz
+      return char;
+    }
+  }
+  return null; // eğer hiçbir karakter benzersiz değilse null döner
+}
+console.log(firstUnlimitedCharacter('swiss'));
+console.log(firstUnlimitedCharacter('hello'));
+console.log(firstUnlimitedCharacter('aabbcc'));
